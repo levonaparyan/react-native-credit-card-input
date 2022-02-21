f0e2704d849787b5e39b4f57df356114d0ce5de3
Levon <levonaparyan97@gmail.com>
2022-02-21 23:21:29 +0400

    ui

--- src/CreditCardInput.js
+++ src/CreditCardInput.js
@@ -33,7 +33,7 @@ const s = StyleSheet.create({
   },
 });
 
-const CVC_INPUT_WIDTH = '49%';
+const CVC_INPUT_WIDTH = 160;
 const EXPIRY_INPUT_WIDTH = CVC_INPUT_WIDTH;
 const CARD_NUMBER_INPUT_WIDTH_OFFSET = 40;
 const CARD_NUMBER_INPUT_WIDTH = Dimensions.get("window").width - EXPIRY_INPUT_WIDTH - CARD_NUMBER_INPUT_WIDTH_OFFSET;
